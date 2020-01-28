8739890855ccf779340db142b4fbd205550a7ffe
WorkPC <10rubleqow@gmail.com>
2020-01-28 17:28:03 +0300

    Продолжаю делать домашнее задание по личному проекту

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var MIN_AVATARS = 1;
 var MAX_AVATARS = 6;
 var MIN_LIKES = 15;
 var MAX_LIKES = 200;
+var DESCRIPTIONS_AMOUNT = 25;
 
 var getRandomArrayElem = function (array) {
   return array[Math.floor(Math.random() * array.length)];
@@ -11,6 +12,10 @@ var getRandomArrayElem = function (array) {
 
 var getRandomNumber = function (minAmount, maxAmount) {
   return Math.round(Math.random() * (maxAmount - minAmount) + minAmount);
+};
+
+var getLinks = function (amountLinks) {
+  return 'photos/' + amountLinks + '.jpg';
 };
 
 var getComments = function (avatar, message, name) {
@@ -36,23 +41,17 @@ var messages = [
 
 var names = ['Игнат', 'Валера', 'Вася', 'Глаша', 'Зоя', 'Эльвира'];
 
-var commentsArr = [];
-
-for (var i = 0; i < 3; i++) {
-  commentsArr.push(getComments(getAvatar(MIN_AVATARS, MAX_AVATARS), messages, names));
-}
-
-var getDescriptions = function (url, desc, comments) {
+var getDescriptions = function (url) {
   return {
-    url: 'здесь будет функция!',
-    desc: '',
+    url: getLinks(url + 1),
+    description: '',
     likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-    comments: 'здесь будет массив!'
+    comments: getComments(getAvatar(MIN_AVATARS, MAX_AVATARS), messages, names)
   };
 };
 
 var description = [];
 
-for (var j = 0; j < 25; j++) {
-  description.push(getDescriptions());
+for (var j = 0; j < DESCRIPTIONS_AMOUNT; j++) {
+  description.push(getDescriptions(j));
 }
