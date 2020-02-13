86145c932d9aaad36985b690a40a263abcce620f
WorkPC <10rubleqow@gmail.com>
2020-02-13 11:10:01 +0300

    Исправил все замечания наставника

--- js/form.js
+++ js/form.js
@@ -10,11 +10,12 @@
   var imgUploadOverlayCloseButton = window.gallery.picturesContainer.querySelector('.cancel');
   var scaleValue = document.querySelector('.scale__control--value');
   var image = window.slider.picture;
+  var effectLevel = document.querySelector('.effect-level');
+  var radioFilters = document.querySelectorAll('.effects__radio');
 
   var onEscapePictureOverlayKeydown = function (evt) {
     if (evt.key === window.preview.ESC_KEY) {
       onUploadFileCloseChange();
-      resetFilterAndScale();
     }
   };
 
@@ -22,6 +23,7 @@
     imgUploadOverlay.classList.remove('hidden');
     document.body.classList.add('modal-open');
     document.addEventListener('keydown', onEscapePictureOverlayKeydown);
+    resetFilterAndScale();
   };
 
   var onUploadFileCloseChange = function () {
@@ -29,12 +31,15 @@
     document.body.classList.remove('modal-open');
     document.removeEventListener('keydown', onEscapePictureOverlayKeydown);
     uploadFile.value = '';
-    resetFilterAndScale();
   };
 
   var resetFilterAndScale = function () {
     scaleValue.value = DEFAULT_SCALE_VALUE;
     image.style.transform = 'scale(' + (parseInt(scaleValue.value, RADIX) / DEFAULT_FACTOR) + ')';
+    image.style.filter = '';
+    image.classList.remove('effects__preview--chrome', 'effects__preview--sepia', 'effects__preview--marvin', 'effects__preview--phobos', 'effects__preview--heat');
+    effectLevel.classList.add('hidden');
+    radioFilters[0].checked = 'checked';
   };
 
   uploadFile.addEventListener('change', onUploadFileChange);
