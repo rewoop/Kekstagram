16b0f74493e8de5318c9741b5f7702873cdc1d73
WorkPC <10rubleqow@gmail.com>
2020-02-18 16:17:04 +0300

    Начал выполнение домашнего задания по личному проекту

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,23 @@
 'use strict';
 
 (function () {
+  var MAX_RANDOM_PHOTOS = 10;
+
   var picture = document.querySelector('#picture').content.querySelector('.picture');
   var picturesContainer = document.querySelector('.pictures');
   var main = document.querySelector('main');
   var errorPopup = document.querySelector('#error').content.querySelector('.error');
   var errorTitle = errorPopup.querySelector('.error__title');
   var errorBtn = errorPopup.querySelector('.error__button');
+  var imageFilters = document.querySelector('.img-filters');
+  var defaultFilter = imageFilters.querySelector('#filter-default');
+  var randomFilter = imageFilters.querySelector('#filter-random');
+  var discussedFilter = imageFilters.querySelector('#filter-discussed');
+  var picturesArray = picturesContainer.querySelectorAll('.picture');
+
+  var getRandomArrayElem = function (array) {
+    return array[Math.floor(Math.random() * array.length)];
+  };
 
   var getPhoto = function (description) {
     var photoDescription = picture.cloneNode(true);
@@ -16,13 +27,67 @@
     return photoDescription;
   };
 
-  var successHandler = function (photos) {
+  var render = function (picture) {
     var fragment = document.createDocumentFragment();
-    photos.forEach(function (item) {
+    picture.forEach(function (item) {
       fragment.appendChild(getPhoto(item));
     });
     picturesContainer.appendChild(fragment);
-    window.preview.chooseBigPicture(picturesContainer.querySelectorAll('.picture'));
+    window.preview.chooseBigPicture(picturesArray);
+  };
+
+  var photos = [];
+  var randomPhotos = [];
+
+  var deleteActiveClass = function () {
+    defaultFilter.classList.remove('img-filters__button--active');
+    randomFilter.classList.remove('img-filters__button--active');
+    discussedFilter.classList.remove('img-filters__button--active');
+
+    picturesArray.forEach(function (item) {
+      item.parentNode.removeChild(item);
+    });
+  };
+
+  var checkPhotosDuplicate = function (photos) {
+    return photos.some(function (item) {
+      return photos.indexOf(item) !== photos.lastIndexOf(item);
+    });
+  };
+
+
+  var filterPhoto = function () {
+    render(photos);
+
+    defaultFilter.addEventListener('click', function () {
+      deleteActiveClass();
+      defaultFilter.classList.add('img-filters__button--active');
+    });
+
+    randomFilter.addEventListener('click', function () {
+      deleteActiveClass();
+      randomPhotos = [];
+      randomFilter.classList.add('img-filters__button--active');
+      for (var i = 0; i < MAX_RANDOM_PHOTOS; i++) {
+        randomPhotos.push(photos[i])
+        // if (!(checkPhotosDuplicate(randomPhotos))) {
+        //
+        // }
+      }
+      render(randomPhotos);
+    });
+
+    discussedFilter.addEventListener('click', function () {
+      deleteActiveClass();
+      discussedFilter.classList.add('img-filters__button--active');
+    });
+  };
+
+
+  var successHandler = function (data) {
+    imageFilters.classList.remove('img-filters--inactive');
+    photos = data;
+    filterPhoto();
   };
 
   var errorHandler = function (errorMessage) {
